5a5be999ed99507aa181af1f02e0d23eb9df348b
Stefan Prodan <stefan.prodan@gmail.com>
2017-01-07 15:59:00 +0200

    generate nginx logs

--- test.sh
+++ test.sh
@@ -5,4 +5,8 @@ hostIP="$(hostname -I|awk '{print $1}')"
 
 docker run -dp 80:80 --name nginx --log-driver=gelf --log-opt gelf-address=udp://${hostIP}:12201 nginx
 
+# generate nginx logs
+curl -s http://hostIP
+curl -s http://hostIP/404
+
 
