95d550e057e957049315f0d4c91e49ab997bc4b4
Stefan Prodan <stefan.prodan@gmail.com>
2017-01-07 17:23:46 +0200

    wait for nginx to start

--- test.sh
+++ test.sh
@@ -5,6 +5,9 @@ hostIP="$(hostname -I|awk '{print $1}')"
 
 docker run -dp 80:80 --name nginx --log-driver=gelf --log-opt gelf-address=udp://${hostIP}:12201 nginx
 
+# wait for nginx
+sleep 1
+
 # generate nginx logs
 curl -s http://hostIP
 curl -s http://hostIP/404
